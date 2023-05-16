cd3de821f3f1de2a94266cd0b343a6724dba01d5
Mostafa-99 <59765480+Mostafa-99@users.noreply.github.com>
2023-05-16 21:24:47 +0300

    added some products

--- building-scripts/productsPage.js
+++ building-scripts/productsPage.js
@@ -1,91 +1,82 @@
 const products = [
     {
         'name': {
-            en:"Chicken",
-            ar: "فراخ"
+            en:"Whole Turkey",
+            ar: "الديك الرومي الكامل"
         },
         'description': {
-            en:"Best chicken in the farm",
-            ar:"احلا فراخ",
+            en:"Premium, tender whole turkeys; hygienically processed and air-chilled",
+            ar:"ديك رومي كامل عالي الجودة، يتم معالجته وفقًا لأعلى معايير النظافة ويتم تبريده بالهواء للحفاظ على الطعم الطازج",
         },
-        'price': {
+        /*'price': {
             en:"500 EGP",
             ar:"500 جم",
+        },*/
+        'img': "assets/img/800x600.png"
+    },
+    {
+        'name': {
+            en:"Turkey Breast",
+            ar: "صدر الديك الرومي"
+        },
+        'description': {
+            en:"Lean, boneless, skinless; versatile for grilling, baking, or sandwiches",
+            ar:"صدر الديك الرومي خالي من العظام والجلد ومنزوع الدهون، مثالي للشواء، الخبز، أو تقطيعه للسندويشات",
         },
         'img': "assets/img/800x600.png"
     },
     {
         'name': {
-            en:"Chicken2",
-            ar: "2فراخ"
+            en:"Turkey Drumsticks",
+            ar: "دبوس الديك الرومي"
         },
         'description': {
-            en:"2Best chicken in the farm",
-            ar:"2احلا فراخ",
-        },
-        'price': {
-            en:"500 EGP",
-            ar:"500 جم",
+            en:"Juicy dark meat; ideal for roasting, smoking, or slow-cooking",
+            ar:"أرجل الديك الرومي العصيرية والطرية مثالية للشواء، التدخين، أو الطبخ البطيء",
         },
         'img': "assets/img/800x600.png"
     },
     {
         'name': {
-            en:"Chicken",
-            ar: "فراخ"
+            en:"Turkey Thighs",
+            ar: "فخذ الديك الرومي"
         },
         'description': {
-            en:"Best chicken in the farm",
-            ar:"احلا فراخ",
-        },
-        'price': {
-            en:"500 EGP",
-            ar:"500 جم",
+            en:"Succulent, bone-in, skin-on; perfect for roasting, braising, or stews",
+            ar:"فخذ الديك الرومي مع العظم والجلد مثالي للشواء، الحساء، أو الطبخ البطيء",
         },
         'img': "assets/img/800x600.png"
     },
     {
         'name': {
-            en:"Chicken",
-            ar: "فراخ"
+            en:"Turkey Wings",
+            ar: "أجنحة الديك الرومي"
         },
         'description': {
-            en:"Best chicken in the farm",
-            ar:"احلا فراخ",
-        },
-        'price': {
-            en:"500 EGP",
-            ar:"500 جم",
+            en:"Meaty, flavorful white meat; suitable for grilling, baking, or slow-cooking",
+            ar:"أجنحة الديك الرومي اللذيذة والممتلئة مثالية للشواء، الخبز، أو الطبخ البطيء و تشكل اختيار ممتاز للمقبلات في المطاعم",
         },
         'img': "assets/img/800x600.png"
     },
     {
         'name': {
-            en:"Chicken",
-            ar: "فراخ"
+            en:"Turkey Necks",
+            ar: "أعناق الديك الرومي"
         },
         'description': {
-            en:"Best chicken in the farm",
-            ar:"احلا فراخ",
-        },
-        'price': {
-            en:"500 EGP",
-            ar:"500 جم",
+            en:"Tasty, nutritious choice; perfect for stocks, broths, or slow-cooked meals",
+            ar:"أعناق الديك الرومي طيبة وغذائية مثالية لصنع المرق أو الحساء أو الطبخ البطيء و لها العديد من الاستخدامات في الصناعات الغذائية",
         },
         'img': "assets/img/800x600.png"
     },
     {
         'name': {
-            en:"Chicken",
-            ar: "فراخ"
+            en:"Turkey Giblets",
+            ar: "أحشاء الديك الرومي"
         },
         'description': {
-            en:"Best chicken in the farm",
-            ar:"احلا فراخ",
-        },
-        'price': {
-            en:"500 EGP",
-            ar:"500 جم",
+            en:"Nutrient-dense organ meats; ideal for gravy, stuffing",
+            ar:"أحشاء الديك الرومي، التي تشمل القلب والكبد و القوانص ، تقدم خيارًا لذيذًا وغنيًا بالعناصر الغذائية لأولئك الذين يقدرون النكهات الفريدة لأعضاء الجسم الداخلية",
         },
         'img': "assets/img/800x600.png"
     },
@@ -98,9 +89,16 @@ const products = [
             en:"Best chicken in the farm",
             ar:"احلا فراخ",
         },
-        'price': {
-            en:"500 EGP",
-            ar:"500 جم",
+        'img': "assets/img/800x600.png"
+    },
+    {
+        'name': {
+            en:"Chicken",
+            ar: "فراخ"
+        },
+        'description': {
+            en:"Best chicken in the farm",
+            ar:"احلا فراخ",
         },
         'img': "assets/img/800x600.png"
     },
@@ -113,10 +111,6 @@ const products = [
             en:"Best chicken in the farm",
             ar:"احلا فراخ",
         },
-        'price': {
-            en:"500 EGP",
-            ar:"500 جم",
-        },
         'img': "assets/img/800x600.png"
     },
     {
@@ -128,10 +122,6 @@ const products = [
             en:"Best chicken in the farm",
             ar:"احلا فراخ",
         },
-        'price': {
-            en:"500 EGP",
-            ar:"500 جم",
-        },
         'img': "assets/img/800x600.png"
     },
     {
@@ -142,40 +132,6 @@ const products = [
         'description': {
             en:"Best chicken in the farm",
             ar:"احلا فراخ",
-        },
-        'price': {
-            en:"500 EGP",
-            ar:"500 جم",
-        },
-        'img': "assets/img/800x600.png"
-    },
-    {
-        'name': {
-            en:"Chicken",
-            ar: "فراخ"
-        },
-        'description': {
-            en:"Best chicken in the farm",
-            ar:"احلا فراخ",
-        },
-        'price': {
-            en:"500 EGP",
-            ar:"500 جم",
-        },
-        'img': "assets/img/800x600.png"
-    },
-    {
-        'name': {
-            en:"Chicken",
-            ar: "فراخ"
-        },
-        'description': {
-            en:"Best chicken in the farm",
-            ar:"احلا فراخ",
-        },
-        'price': {
-            en:"500 EGP",
-            ar:"500 جم",
         },
         'img': "assets/img/800x600.png"
     },
@@ -221,14 +177,15 @@ const buildProducts = () =>{
             '    <div class="item">'+
             '        <div class="thumb">'+
             '            <img src="assets/img/800x600.png" alt="Thumb">'+
-            '           <div class="date" '+(language==="en" ? "" : 'style="right:unset;left:30px;"')+'>'+item.price[language]+'</div>'+
+            //'           <div class="date" '+(language==="en" ? "" : 'style="right:unset;left:30px;"')+'>'+item.price[language]+'</div>'+
+            '           <div class="date" '+(language==="en" ? "" : 'style="right:unset;left:30px;"')+'>'+item.name[language]+'</div>'+
             '       </div>'+
             '       <div class="info">'+
-            '           <h4>'+
+           /* '           <h4>'+
             '               '+item.name[language]+''+
             //'               <a href="#">Chicken</a>'+
-            '           </h4>'+
-            '           <p>'+
+            '           </h4>'+*/
+            '           <p style="height:90px;">'+
             '               '+item.description[language]+''+
             '           </p>'+
             
